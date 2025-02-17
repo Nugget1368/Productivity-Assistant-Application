827bfdd6dde62c06ce56e2d00b5d52ef55f22806
Nugget1368 <rneg1@hotmail.com>
2025-02-17 11:55:32 +0100

    Function to delete from localstorage

--- services/localstorage.js
+++ services/localstorage.js
@@ -1,22 +1,29 @@
 const ACTIVITIES_KEY = "activities";
 
-const saveToStorage = (storageName, obj) =>{
-    // If there is localstorage with key-string 'storageName'
-    if(localStorage.getItem(storageName)){
-        // Get value from localstorage and convert from JSON to array-object
-        let storage = JSON.parse(localStorage.getItem(storageName));
-        storage.push(obj);
-        localStorage.setItem(storageName, JSON.stringify(storage));
-    }
-    // If there is no localstorage with the key-string
-    else{
-        let arr = [];
-        arr.push(obj);
-        //Convert object to JSON and save in localstorage
-        localStorage.setItem(storageName, JSON.stringify(arr));
-    }   
-}
+const getStorageAsJSON = (storageName) =>
+  JSON.parse(localStorage.getItem(storageName));
 
-const getStorageAsJSON = (storageName)=> JSON.parse(localStorage.getItem(storageName));
+const saveToStorage = (storageName, obj) => {
+  // If there is localstorage with key-string 'storageName'
+  if (localStorage.getItem(storageName)) {
+    // Get value from localstorage and convert from JSON to array-object
+    let storage = getStorageAsJSON(storageName);
+    storage.push(obj);
+    localStorage.setItem(storageName, JSON.stringify(storage));
+  }
+  // If there is no localstorage with the key-string
+  else {
+    let arr = [];
+    arr.push(obj);
+    //Convert object to JSON and save in localstorage
+    localStorage.setItem(storageName, JSON.stringify(arr));
+  }
+};
 
-export {saveToStorage, ACTIVITIES_KEY}
+const deleteFromStorage = (storageName, id) => {
+  let storage = getStorageAsJSON(storageName);
+  storage = storage.filter((element) => element.id !== id);
+  localStorage.setItem(ACTIVITIES_KEY, JSON.stringify(storage));
+};
+
+export { saveToStorage, deleteFromStorage, getStorageAsJSON, ACTIVITIES_KEY };
