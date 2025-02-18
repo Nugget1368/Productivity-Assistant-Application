e97d5cbf8e121526caf9612bc302444f944a2e9d
Nugget1368 <rneg1@hotmail.com>
2025-02-18 11:48:28 +0100

    updated categories into objects and removed test-code

--- main.js
+++ main.js
@@ -1,6 +1,2 @@
-import { loadFromJSONAsync, CATEGORIES_KEY } from "./services/jsonHandler.js"
 import {saveToStorage, deleteFromStorage, ACTIVITIES_KEY, getStorageAsJSON, editStorage} from "./services/localstorage.js"
 import {createTodo} from "./services/todoActivities.js"
-
-let test = await loadFromJSONAsync(CATEGORIES_KEY);
-console.log(test);
