ad6f8d78efe28e2d3e6f38aaf431f63116996bbb
Robert Grimsborn <112860814+Nugget1368@users.noreply.github.com>
2025-02-21 10:20:32 +0100

    Robert/open popup (#14)
    Modal med dialogruta som inehåller ett formulär för att kunna lägga till ärenden för respektive sida.
    
    Notera att detta är lite hastigt byggt och behöver snyggas till med builder och enhetlig funktion för formulärets validering, kort är upplagda.
    
    * Open and Close Modal
    
    * Added Mockdata in form-builder for todo
    
    * updated popup header to h3
    
    * Simple form without proper validation for todos
    
    * Updated css for modal
    
    * Popup-finctionality to add Habits and events, copied and pasted from todo.js
    
    * css styling
    
    * Error handling for empty storage

--- pages-js/habits.js
+++ pages-js/habits.js
@@ -5,4 +5,33 @@ import { buildHabit } from "../builders/habitBuilder.js";
 
 //Create Habits in DOM
 let storage = getStorageAsJSON(HABITS_KEY);
-buildHabit(storage);
+if(storage){
+  buildHabit(storage);
+}
+
+
+const submitForm = () => {
+    let inputs = document.querySelectorAll("form#create-habit input");
+    let values = [];
+    inputs.forEach((input) => {
+      values.push(input.value);
+    });
+    let habit = createHabit(values[0], values[1]);
+    saveToStorage(HABITS_KEY, habit);
+  };
+  
+  let form = document.querySelector("form#create-habit");
+  form.addEventListener("submit", () => submitForm());
+  
+  const openModalBtn = document.querySelector("[open-modal]");
+  const closeModalBtn = document.querySelector("[close-modal]");
+  const modal = document.querySelector("[modal]");
+  
+  openModalBtn.addEventListener("click", () => {
+    // buildTodosForm();
+    modal.showModal();
+  });
+  
+  closeModalBtn.addEventListener("click", () => {
+    modal.close();
+  });
