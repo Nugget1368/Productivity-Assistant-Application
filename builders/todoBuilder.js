3ab8d057a92294c38e949455e3ac90f34e9137cc
NathalieBruno <144827786+NathalieBruno@users.noreply.github.com>
2025-02-24 11:45:25 +0100

    added styling for todo activities that are added and also hover/active functions. also added JS function for this and updated buildTodos function. (#17)

--- builders/todoBuilder.js
+++ builders/todoBuilder.js
@@ -1,6 +1,9 @@
 const buildTodos = (todos) => {
   let ul = document.querySelector("#todos ul");
   todos.forEach((todo) => {
+    let wrapperDiv = document.createElement("div");
+    wrapperDiv.classList.add("todo-wrapper");
+
     let li = document.createElement("li");
     let checkbox = document.createElement("input");
     let title = document.createElement("label");
@@ -11,12 +14,19 @@ const buildTodos = (todos) => {
     checkbox.checked = todo.status;
     title.textContent = todo.title;
     title.setAttribute("for", todo.id);
-    deadline = todo.deadline;
-    li.append(checkbox, title, deadline);
+    deadline.textContent = "Deadline: " + todo.deadline;
+    li.append(wrapperDiv, deadline);
+    wrapperDiv.append(checkbox, title);
     ul.append(li);
+    li.addEventListener("click", function () {
+      let selected = document.querySelector("#todos ul li.selected");
+      if (selected && selected !== li) {
+        selected.classList.remove("selected");
+      }
+      li.classList.toggle("selected");
+    });
   });
 };
-
 const buildTodosForm = () => {
   let modal = document.querySelector("dialog[modal] article");
   let h3 = document.querySelector("dialog[modal] h3");
@@ -26,19 +36,19 @@ const buildTodosForm = () => {
   titleLabel.textContent = "Titel";
   let title = document.createElement("input");
   titleDiv.append(titleLabel, title);
-  
+
   let descriptionDiv = document.createElement("div");
   let descriptionLabel = document.createElement("label");
   descriptionLabel.textContent = "Beskrivning";
   let description = document.createElement("input");
   descriptionDiv.append(descriptionLabel, description);
-  
+
   let timeDiv = document.createElement("div");
   let timeLabel = document.createElement("label");
   timeLabel.textContent = "Tidsestimering";
   let time = document.createElement("input");
   timeDiv.append(timeLabel, time);
-  
+
   let categoryDiv = document.createElement("div");
   let categoryLabel = document.createElement("label");
   categoryLabel.textContent = "Kategori";
