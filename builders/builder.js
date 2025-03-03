06e32d8949863e5e82951eacbd0c26088b2c19f0
Robert Grimsborn <112860814+Nugget1368@users.noreply.github.com>
2025-03-03 11:38:45 +0100

    Robert/modal design (#33)
    * Styling for dialog
    
    * Updated styling for inputs

--- builders/builder.js
+++ builders/builder.js
@@ -5,12 +5,16 @@ export const formBuilder = (destination = "", formId = "") => {
     dest.append(form);
     let submitBtn = document.createElement("button");
     submitBtn.setAttribute("type", "submit");
+    submitBtn.classList.add("add-button");
     submitBtn.textContent = "Lägg till";
     let cancelBtn = document.createElement("button");
     cancelBtn.setAttribute("id", "cancel-btn");
     cancelBtn.customAttribute = "close-modal"
+    cancelBtn.classList.add("delete-btn");
     cancelBtn.textContent = "Avbryt";
-    form.append(cancelBtn, submitBtn);
+    let footer = document.createElement("footer");
+    footer.append(submitBtn, cancelBtn);
+    form.append(footer);
     //Cancel submit
     cancelBtn.addEventListener("click", (event) => {
         event.preventDefault();
