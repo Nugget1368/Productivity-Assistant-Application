4dd8553d8ed4cfdfc0f6d58af7f5d547b5ff4081
Daniel Hollstedt <126522750+Hollstedt@users.noreply.github.com>
2025-03-04 11:03:10 +0100

    Nathalie daniel/funktionellt kunna ta bort ett arenden (#36)
    * added "selected-item" attribute to highlight chosen todo and get todo-ID
    
    * added "selected-item" attribute to highlight chosen todo and get todo-ID
    
    * added functionality to delete buttom from open modal that deletes todo from localStorage and also added a function which sets visibility hidden if no li exists under ul in cards list
    
    * if no description was given on todo, the setAttribute wasnt passed on to todos-right
    
    * added functionality to delete-btn on eventpage
    
    * Replaced appendChild with append
    
    ---------
    
    Co-authored-by: NathalieBruno <144827786+NathalieBruno@users.noreply.github.com>
    Co-authored-by: Nugget1368 <rneg1@hotmail.com>

--- builders/builder.js
+++ builders/builder.js
@@ -1,24 +1,24 @@
 export const formBuilder = (destination = "", formId = "") => {
-    let dest = document.querySelector(destination); //dialog[modal] article
-    let form = document.createElement("form");
-    form.setAttribute("id", formId)
-    dest.append(form);
-    let submitBtn = document.createElement("button");
-    submitBtn.setAttribute("type", "submit");
-    submitBtn.classList.add("add-button");
-    submitBtn.textContent = "Lägg till";
-    let cancelBtn = document.createElement("button");
-    cancelBtn.setAttribute("id", "cancel-btn");
-    cancelBtn.customAttribute = "close-modal"
-    cancelBtn.classList.add("delete-btn");
-    cancelBtn.textContent = "Avbryt";
-    let footer = document.createElement("footer");
-    footer.append(submitBtn, cancelBtn);
-    form.append(footer);
-    //Cancel submit
-    cancelBtn.addEventListener("click", (event) => {
-        event.preventDefault();
-        let modal = document.querySelector("dialog[modal]");
-        modal.close();
-    });
-}
+  let dest = document.querySelector(destination); //dialog[modal] article
+  let form = document.createElement("form");
+  form.setAttribute("id", formId);
+  dest.append(form);
+  let submitBtn = document.createElement("button");
+  submitBtn.setAttribute("type", "submit");
+  submitBtn.classList.add("add-button");
+  submitBtn.textContent = "Lägg till";
+  let cancelBtn = document.createElement("button");
+  cancelBtn.setAttribute("id", "cancel-btn");
+  cancelBtn.customAttribute = "close-modal";
+  cancelBtn.classList.add("delete-btn");
+  cancelBtn.textContent = "Avbryt";
+  let footer = document.createElement("footer");
+  footer.append(submitBtn, cancelBtn);
+  form.append(footer);
+  //Cancel submit
+  cancelBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    let modal = document.querySelector("dialog[modal]");
+    modal.close();
+  });
+};
