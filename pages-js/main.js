725daa6af8de2c5c68ccd54628318be2123f5470
NathalieBruno <144827786+NathalieBruno@users.noreply.github.com>
2025-03-05 16:29:13 +0100

    added day + date + month to startpage

--- pages-js/main.js
+++ pages-js/main.js
@@ -1,1 +1,32 @@
-import {saveToStorage, deleteFromStorage, ACTIVITIES_KEY, getStorageAsJSON, editStorage} from "../services/localstorage.js"
+import {
+  saveToStorage,
+  deleteFromStorage,
+  ACTIVITIES_KEY,
+  getStorageAsJSON,
+  editStorage,
+} from "../services/localstorage.js";
+
+const weekday = ["Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag"];
+const monthNames = [
+  "januari",
+  "februari",
+  "mars",
+  "april",
+  "maj",
+  "juni",
+  "juli",
+  "augusti",
+  "september",
+  "oktober",
+  "november",
+  "december",
+];
+
+const d = new Date();
+let day = weekday[d.getDay()];
+let date = d.getDate();
+let month = monthNames[d.getMonth()];
+
+let fullDate = `${day} ${date} ${month}`;
+
+document.querySelector(".h2-startpage").textContent = fullDate;
