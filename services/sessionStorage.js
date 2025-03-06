0cbfc4eef924859dc8810ce3ea32c2c0cd14603d
Nugget1368 <rneg1@hotmail.com>
2025-03-06 10:28:21 +0100

    removed error

--- services/sessionStorage.js
+++ services/sessionStorage.js
@@ -1,2 +1,2 @@
 export const setSessionStorage = (storageName = "", obj = {}) => sessionStorage.setItem(storageName, JSON.stringify(obj));
-export const getSessionStorage = (storageName = "", obj = {}) => JSON.parse(sessionStorage.getItem(storageName));
+export const getSessionStorage = (storageName = "") => JSON.parse(sessionStorage.getItem(storageName));
